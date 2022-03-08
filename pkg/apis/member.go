d76f0ed0fe36380abd90e50bd3233aced4910cc5
jie.li <jie.li@daocloud.io>
2022-03-08 15:03:33 +0800

    fix: update csi driver name and topology key
    Signed-off-by: jie.li <jie.li@daocloud.io>

--- pkg/apis/member.go
+++ pkg/apis/member.go
@@ -16,7 +16,7 @@ const (
 
 	ControllerLeaseName = "localstorage-local-storage-master"
 
-	TopologyNodeKey = "localstorage.hwameistor.io/local-storage-topology-node"
+	TopologyNodeKey = "topology.lvm.hwameistor.io/node"
 )
 
 // LocalStorageMember interface
